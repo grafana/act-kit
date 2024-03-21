a7e3df7616b551790f2181ef8c05a5db9270fc11
Dominik Süß <dominik@suess.wtf>
2024-03-21 13:48:54 +0100

    refactor: switch to aws state

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ terraform {
       version = "2.14.2"
     }
   }
+
+  # =====================================
+  # | TODO: CHANGE THIS TO YOUR BACKEND |
+  # =====================================
+
+  backend "s3" {
+    bucket = "grafanalabs-act-kit-tfstate"
+    key    = "act-kit/tfstate-gh-actions"
+    region = "us-west-2"
+  }
 }
 
 provider "grafana" {
@@ -40,7 +50,7 @@ module "schedule" {
   source = "./schedules/simple-rotation"
   team_members = [
     "dominiksuess",
-    "dominiksuessdevextest",
+    # "ishanjain",
   ]
 }
 
@@ -50,9 +60,9 @@ module "schedule" {
 #     "dominiksuess",
 #   ]
 #   team_members_apac = [
-#     "dominiksuessapacuser",
+#     "ishanjain",
 #   ]
 #   team_members_amer = [
-#     "dominiksuessdevextest",
+#     "serena",
 #   ]
 # }
